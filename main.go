ee8a162bcb142a7e421a899899f113398ba8f795
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-26 15:37:18 +0530

    basic implementation

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
-package me
+package main
+
+import (
+	//"Me/tokens"
+	"fmt"
+	"os"
+	//"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No input file provided.")
+		fmt.Println("Usage: ./program_name <filename>")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Multiple files found.")
+		fmt.Println("Usage: ./program_name <filename>")
+		os.Exit(1)
+	}
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:",err)
+		return
+	}
+	
+}
